6433937010b88f83c465893f2baa0b8fb6cd7889
Smasher Guy <tejasmishra18302@gmail.com>
2026-09-21 23:17:08 +0530

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,15 @@
+export interface PersonalInfo {
+  age: string;
+  realName: string;
+  occupation: string;
+  hobbies: string;
+}
+
+export interface CustomImages {
+  avatar: string;
+  mainRobot: string;
+  floatTopLeft: string;
+  floatBottomLeft: string;
+  floatTopRight: string;
+  floatBottomRight: string;
+}
